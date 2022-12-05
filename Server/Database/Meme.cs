1339638bd50f3f02b70bf3d62501e4af5b1a1c59
Andrei Stan <standrei2003@yahoo.com>
2022-12-05 19:32:13 +0200

    Created Meme entity.

---
+++ Server/Database/Meme.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MemeIT.Server.Database
+{
+    [PrimaryKey(nameof(MemeId))]
+    public partial class Meme
+    {
+        public Guid MemeId { get; set; }
+
+        public string Description { get; set; } = default!;
+
+        public Guid? CreatorId { get; set; }
+        public virtual ApplicationUser? Creator { get; set; }
+    }
+}
