ca7b2df72945b292d6cb8052a87d590c2695dd37
Andrei Stan <standrei2003@yahoo.com>
2022-12-10 22:03:51 +0200

    Created Meme DTO

---
+++ Shared/Models/MemeDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemeIT.Shared.Models
+{
+    public record struct MemeDTO(Guid? MemeId, string? Description, Guid? CreatorId, string? CreatorUsername);
+}
