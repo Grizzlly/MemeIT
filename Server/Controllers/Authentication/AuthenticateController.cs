3d3e327ab9606311f51182aa074ab0a4d8ca7d15
Andrei Stan <standrei2003@yahoo.com>
2022-12-10 21:57:41 +0200

    Added login endpoint (#19)

--- Server/Controllers/Authentication/AuthenticateController.cs
+++ Server/Controllers/Authentication/AuthenticateController.cs
@@ -3,7 +3,10 @@ using MemeIT.Shared.Models.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using System.Net.Mail;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace MemeIT.Server.Controllers.Authentication
 {
@@ -12,11 +15,15 @@ namespace MemeIT.Server.Controllers.Authentication
     [Route("api/authentication")]
     public class AuthenticateController : ControllerBase
     {
-        private readonly UserManager<ApplicationUser> userManager;
+        private static readonly SymmetricSecurityKey SecurityAppKey = new(Encoding.ASCII.GetBytes("ByYM000OLlMQG6VVVp1OH7Xzyr7gHuw1qvUC5dcGt3SNM"));
 
-        public AuthenticateController(UserManager<ApplicationUser> userManager)
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly IConfiguration configuration;
+
+        public AuthenticateController(UserManager<ApplicationUser> userManager, IConfiguration configuration)
         {
             this.userManager = userManager;
+            this.configuration = configuration;
         }
 
         [HttpPost]
@@ -46,6 +53,50 @@ namespace MemeIT.Server.Controllers.Authentication
             }
 
             return Ok(new AuthResponse { Message = "User created successfully!" });
+        }
+
+        [HttpPost]
+        [Route("login")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login([FromBody] LoginModel loginModel)
+        {
+            ApplicationUser user = (await userManager.FindByNameAsync(loginModel.UserName))!;
+
+            if ((user is not null) && await userManager.CheckPasswordAsync(user, loginModel.Password))
+            {
+                Task<IList<string>> userRoles = userManager.GetRolesAsync(user);
+
+                List<Claim> authClaims = new()
+                {
+                    new Claim(ClaimTypes.Name, user.UserName ?? string.Empty),
+                    new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                    new Claim(Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(ClaimTypes.AuthenticationMethod, "pwd")
+                };
+
+                foreach (string role in await userRoles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, role));
+                }
+
+                SymmetricSecurityKey authSigningKey = SecurityAppKey;
+
+                JwtSecurityToken token = new(
+                    issuer: configuration["JWT:ValidIssuer"],
+                    audience: configuration["JWT:ValidAudience"],
+                    expires: DateTime.Now.AddDays(7),
+                    claims: authClaims,
+                    signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                    );
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo
+                });
+            }
+
+            return Unauthorized();
         }
     }
 }
