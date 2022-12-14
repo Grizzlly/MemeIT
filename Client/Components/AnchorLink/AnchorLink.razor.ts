4246b546d03f286dd92b764d1e8d414ea8995c96
Andrei Stan <standrei2003@yahoo.com>
2022-12-14 23:02:48 +0200

    Improved design (#46)

---
+++ Client/Components/AnchorLink/AnchorLink.razor.ts
@@ -0,0 +1,10 @@
+﻿
+export function ScrollToElementById(elementId: string): void {
+    if (elementId && elementId.length != 0) {
+        const element = document.getElementById(elementId);
+
+        if (element instanceof HTMLElement) {
+            element.scrollIntoView(true);
+        }
+    }
+}
